893e3ecbc31a4cc85346479373d38c1ab066e57d
aysema mermer <aysemamermer99@gmail.com>
2024-01-01 01:49:44 +0300

    Equipment filter

--- src/components/EquipmentForm.vue
+++ src/components/EquipmentForm.vue
@@ -4,18 +4,18 @@
     <div class="container">
       <h2>{{ equipment.id ? 'Edit Equipment' : 'Add Equipment' }}</h2>
       <form @submit.prevent="handleSubmit">
-        <div class="mb-3">
-          <label class="form-label">Name:</label>
-          <input v-model="equipment.name" class="form-control" required>
+        <div class="form-group">
+          <label for="name">Name:</label>
+          <input v-model="formData.name" id="name" class="form-control" required>
         </div>
-        <div class="mb-3">
-          <label class="form-label">Inventory Number:</label>
-          <input v-model="equipment.inventory_number" class="form-control" required>
+        <div class="form-group">
+          <label for="inventory_number">Inventory Number:</label>
+          <input v-model="formData.inventory_number" id="inventory_number" class="form-control" required>
         </div>
-        <div class="mb-3">
-          <label class="form-label">Machine:</label>
-          <select v-model="equipment.machine.id" class="form-control">
-            <option v-for="machineOption in machineOptions" :key="machineOption.id" :value="machineOption.id">
+        <div class="form-group">
+          <label for="machine">Machine:</label>
+          <select v-model="formData.machine" id="machine" class="form-control">
+            <option v-for="machineOption in machineOptions" :key="machineOption.id" :value="machineOption">
               {{ machineOption.name }}
             </option>
           </select>
@@ -26,7 +26,9 @@
           <button type="button" @click="closeForm" class="btn btn-secondary">Cancel</button>
         </div>
       </form>
-      <div class="spacer-form"></div>
+      <div v-if="localErrorMessage" class="alert alert-danger mt-4">
+        {{ localErrorMessage }}
+      </div>
     </div>
   </div>
 </template>
@@ -35,20 +37,20 @@
 import axios from 'axios';
 
 export default {
-  props:{
-    equipment : Object,
+  props: {
+    equipment: Object,
     visible: Boolean,
-    errorMessage : String
+    errorMessage: String,
   },
   data() {
     return {
-      equipment: {
-        id: null,
+      formData: {
         name: '',
         inventory_number: '',
-        machine: { id: null, name: '' },
+        machine: null,
       },
       machineOptions: [],
+      localErrorMessage: '',
     };
   },
   watch: {
@@ -65,159 +67,90 @@ export default {
     this.fetchMachineOptions();
   },
   methods: {
+    populateFormData(newEquipment) {
+      if (newEquipment) {
+        this.formData = { ...newEquipment };
+      } else {
+        this.formData = {
+          name: '',
+          inventory_number: '',
+          machine: null,
+        };
+      }
+    },
+    handleSubmit() {
+      if (this.equipment.id) {
+        this.updateEquipment();
+      } else {
+        this.addEquipment();
+      }
+    },
+    async addEquipment() {
+      try {
+        const response = await axios.post('http://127.0.0.1:8000/api/equipment/', this.formData);
+        this.$emit('add', response.data);
+        this.closeForm();
+      } catch (error) {
+        this.handleApiError(error);
+      }
+    },
+    updateEquipment() {
+      axios
+        .put(`http://127.0.0.1:8000/api/equipment/${this.equipment.id}/`, this.formData)
+        .then((response) => {
+          this.closeForm();
+        })
+        .catch((error) => {
+          this.handleApiError(error);
+        });
+    },
+    closeForm() {
+      this.$emit('close');
+      this.formData = {
+        name: '',
+        inventory_number: '',
+        machine: null,
+      };
+    },
     fetchMachineOptions() {
       axios
-        .get('http://127.0.0.1:8000/api/machines/')
+        .get('http://127.0.0.1:8000/api/machines/')  // Assuming there is an endpoint for fetching machines
         .then((response) => {
           this.machineOptions = response.data;
-
-          // Eğer equipment bir makineye sahipse, bu makineyi equipment içine ekleyin
-          if (this.equipment.machine && this.equipment.machine.id) {
-            const selectedMachine = this.machineOptions.find((machine) => machine.id === this.equipment.machine.id);
-            if (selectedMachine) {
-              this.equipment.machine = selectedMachine;
-            }
-          }
         })
         .catch((error) => {
           console.error('API Error:', error);
         });
     },
-    handleSubmit() {
-      if (this.equipment) {
-        this.updateEquipment();
-      } else {
-        this.saveEquipment();
-      }
+    updateLocalErrorMessage(newErrorMessage) {
+      this.localErrorMessage = newErrorMessage;
     },
-    saveEquipment() {
-  const requestData = {
-    name: this.equipment.name,
-    inventory_number: this.equipment.inventory_number,
-  };
-
-  // Eğer bir makine seçilmişse ve makine nesnesi varsa
-  if (this.equipment.machine && this.equipment.machine.id) {
-    requestData.machine = this.equipment.machine.id;
-  }
-
-  if (this.equipment.id) {
-    // Update existing equipment
-    axios
-      .put(`http://127.0.0.1:8000/api/equipment/${this.equipment.id}/`, requestData)
-      .then((response) => {
-        console.log('Equipment successfully updated:', response.data);
-        this.fetchEquipments();
-        this.closeForm();
-      })
-      .catch((error) => {
-        console.error('PUT API Error:', error);
-      });
-  } else {
-    // Add new equipment
-    axios
-      .post('http://127.0.0.1:8000/api/equipment/', requestData)
-      .then((response) => {
-        console.log('Equipment successfully added:', response.data);
-        this.fetchEquipments();
-        this.closeForm();
-      })
-      .catch((error) => {
-        console.error('POST API Error:', error);
-      });
-  }
-},
-updateEquipment() {
-  const requestData = {
-    name: this.equipment.name,
-    inventory_number: this.equipment.inventory_number,
-  };
-
-  if (this.equipment.machine && this.equipment.machine.id) {
-    requestData.machine = this.equipment.machine.id;
-  }
-
-  axios
-    .put(`http://127.0.0.1:8000/api/equipment/${this.equipment.id}/`, requestData)
-    .then((response) => {
-      this.closeForm();
-      this.fetchEquipments();
-
-      const successMessage = response.data.success_message || 'Equipment updated successfully!';
-      this.successMessage = successMessage;
-      this.successModalVisible = true;
-    })
-    .catch((error) => {
+    handleApiError(error) {
       if (error.response && error.response.status === 400 && error.response.data) {
         const apiErrors = error.response.data;
-        this.localErrorMessage = apiErrors.inventory_number ? apiErrors.inventory_number[0] : 'Something went wrong. Please try again.';
+        if (apiErrors.inventory_number) {
+          this.localErrorMessage = apiErrors.inventory_number[0];
+        } else {
+          this.localErrorMessage = 'Something went wrong. Please try again.';
+        }
       } else if (error.response && error.response.status === 500) {
         this.localErrorMessage = 'Server error. Please try again.';
       } else {
         this.localErrorMessage = 'Something went wrong. Please try again.';
       }
       console.error('API Error:', error);
-    });
-},
-
-    closeForm() {
-      this.$emit('close'); // Notify the parent component to close the form
     },
-    openEditForm() {
-  console.log('Edit button clicked. Equipment:', this.equipment);
-  this.$emit('update', this.equipment); // Değişiklik burada
-},
-editEquipment(equipment) {
-      this.selectedEquipment = equipment;
-      this.isFormVisible = true;
-    },
-
   },
 };
 </script>
+
 <style scoped>
 .spacer {
   margin-top: 20px;
-  /* veya istediğiniz bir boşluk değeri */
 }
 
-.modal {
-  display: none;
-  position: fixed;
-  z-index: 1;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  overflow: auto;
-  background-color: rgba(0, 0, 0, 0.4);
-}
-
-.modal-content {
-  background-color: #fefefe;
-  margin: 15% auto;
-  padding: 20px;
-  border: 1px solid #888;
-  width: 80%;
-}
-
-.close {
-  color: #aaa;
-  float: right;
-  font-size: 28px;
-  font-weight: bold;
-}
-
-.close:hover,
-.close:focus {
-  color: black;
-  text-decoration: none;
-  cursor: pointer;
-}
-
-.spacer-form {
-  margin-top: 50px;
-  /* veya istediğiniz bir boşluk değeri */
+.form-group {
+  margin-bottom: 20px;
 }
 
 .button-container {
@@ -227,6 +160,5 @@ editEquipment(equipment) {
 
 .button-spacing {
   margin-right: 10px;
-  /* İhtiyacınıza göre ayarlayabilirsiniz */
 }
 </style>
--- src/views/Equipments.vue
+++ src/views/Equipments.vue
@@ -3,7 +3,12 @@
     <div>
       <h1>Equipments</h1>
       <button @click="openAddForm" class="btn btn-primary">Add Equipment</button>
+      <div class="mb-3">
+        <label class="form-label">Search:</label>
+        <input v-model="searchText" class="form-control">
+      </div>
       <div class="spacer"></div>
+
       <div class="table-container">
         <table class="table">
           <thead>
@@ -11,14 +16,16 @@
               <th>ID</th>
               <th>Name</th>
               <th>Inventory Number</th>
+              <th>Machine</th>
               <th>Actions</th>
             </tr>
           </thead>
           <tbody>
-            <tr v-for="equipment in equipments" :key="equipment.id">
+            <tr v-for="equipment in filteredEquipments" :key="equipment.id">
               <td>{{ equipment.id }}</td>
               <td>{{ equipment.name }}</td>
               <td>{{ equipment.inventory_number }}</td>
+              <td>{{ equipment.machine ? equipment.machine.name : 'N/A' }}</td>
               <td>
                 <div class="button-container">
                   <button @click="editEquipment(equipment)" class="btn btn-warning">Edit</button>
@@ -35,23 +42,23 @@
     <div v-if="errorMessage" class="alert alert-danger mt-4">
       {{ errorMessage }}
     </div>
-    <!-- Başarı Mesajı -->
+    
     <div v-if="successMessage" class="alert alert-success mt-4">
       {{ successMessage }}
     </div>
-
+    
     <equipment-form
-  :equipment="selectedEquipment"
-  :visible="isFormVisible"
-  @add="addEquipment"
-  @update="updateEquipment" 
-  @close="closeForm"
-  :error-message="errorMessage"
-  :success-message="successMessage"
-></equipment-form>
-
+      :equipment="selectedEquipment"
+      :visible="isFormVisible"
+      @add="addEquipment"
+      @update="updateEquipment" 
+      @close="closeForm"
+      :error-message="errorMessage"
+      :success-message="successMessage"
+    ></equipment-form>
   </div>
 </template>
+
 <script>
 import axios from 'axios';
 import EquipmentForm from '@/components/EquipmentForm.vue';
@@ -64,23 +71,31 @@ export default {
       selectedEquipment: null,
       errorMessage: '',
       successMessage: '',
+      searchText: '',
     };
+  },
+  computed: {
+    filteredEquipments() {
+      return this.equipments.filter(equipment => 
+        equipment.name.toLowerCase().includes(this.searchText.toLowerCase()) ||
+        equipment.inventory_number.includes(this.searchText)
+      );
+    }
   },
   mounted() {
     this.fetchEquipments();
   },
   methods: {
     fetchEquipments() {
-    axios
-      .get('http://127.0.0.1:8000/api/equipment/')
-      .then((response) => {
-        this.equipments = response.data;
-      })
-      .catch((error) => {
-        console.error('API Error:', error);
-      });
-  },
-
+      axios
+        .get('http://127.0.0.1:8000/api/equipment/')
+        .then((response) => {
+          this.equipments = response.data;
+        })
+        .catch((error) => {
+          console.error('API Error:', error);
+        });
+    },
     openAddForm() {
       this.selectedEquipment = null;
       this.isFormVisible = true;
@@ -92,7 +107,7 @@ export default {
     deleteEquipment(equipmentId) {
       if (confirm('Are you sure you want to delete this equipment?')) {
         axios
-          .delete(`http://127.0.0.1:8000/api/equipment/${equipmentId}/`)
+          .delete(`http://127.0.0.1:8000/api/equipment/${equipmentId}/delete/`)
           .then(() => {
             this.fetchEquipments();
           })
@@ -104,19 +119,16 @@ export default {
     closeForm() {
       this.isFormVisible = false;
       this.selectedEquipment = null;
-      this.successMessage = ''; // Başarı mesajını sıfırla
+      this.successMessage = '';
     },
     addEquipment() {
       this.successMessage = 'Equipment added successfully!';
       this.fetchEquipments();
     },
     updateEquipment() {
-    this.successMessage = 'Equipment updated successfully!';
-    this.fetchEquipments();
-    this.successModalVisible = true;
-  },
-
-
+      this.successMessage = 'Equipment updated successfully!';
+      this.fetchEquipments();
+    },
   },
   components: {
     EquipmentForm,
@@ -125,21 +137,16 @@ export default {
 </script>
 
 <style scoped>
+.spacer {
+  margin-top: 20px;
+}
+
+.form-group {
+  margin-bottom: 20px;
+}
 
 .table-container {
-  max-height: 300px; 
-  overflow-y: auto;
-  border: 1px solid #ddd; 
-  border-radius: 5px; 
-  overflow: auto; 
-  padding: 10px; 
-
-}
-.spacer-containers {
-  margin-top: 40px; 
-}
-.spacer {
-  margin-top: 20px; 
+  margin-top: 20px;
 }
 
 .button-container {
@@ -148,7 +155,6 @@ export default {
 }
 
 .button-spacing {
-  margin-right: 10px; 
+  margin-right: 10px;
 }
-
 </style>
